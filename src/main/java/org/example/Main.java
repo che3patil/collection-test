122cf807234bedba6155939af20f9de7a441ccbe
Admin <che3patil@gmail.com>
2025-03-22 14:47:22 +0530

    collection test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+
+
+}
